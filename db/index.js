0c93e6d31a6ebab1d2164b6690bae0fb01a475bf
Carlos Perez <cmperez0730@gmail.com>
2024-04-16 21:46:43 -0700

    I spent a hour trying to debug a error which was just a ,

--- db/index.js
+++ db/index.js
@@ -1,5 +1,41 @@
 const client = require('./client.js');
 
+const getUserById = async (id) => {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT * FROM users 
+    WHERE id=$1;
+  `,
+      [id]
+    );
+    return user;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const createUser = async (username, password) => {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    RETURNING *;
+    `,
+      [username, password]
+    );
+
+    return user;
+  } catch (err) {
+    throw err;
+  }
+};
+
 const addCar = async (carYear, carBrand, carModel, carTrim, carImage, carDetails, carPrice) => {
   try {
     await client.query(`
@@ -11,6 +47,51 @@ const addCar = async (carYear, carBrand, carModel, carTrim, carImage, carDetails
   }
 };
 
+const getAllCars = async () => {
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM available;
+    `);
+
+    return rows;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteCar = async (carId) => {
+  try{
+    const {rows: [available]} = await client.query( 
+      `
+        DELETE FROM available
+        WHERE id=${carId}
+        RETURNING *;
+      `
+    )
+    return available;
+  }
+  catch(err){
+    throw err;
+  }
+}
+
+const getAllUsersCar = async (userId) => {
+  try {
+    const { rows } = await client.query(`
+    SELECT * FROM available WHERE addedBy=${userId};
+    `);
+
+    return rows;
+  } catch (err) {
+    throw err;
+  }
+};
+
 module.exports = {
-  addCar
+  createUser,
+  addCar,
+  getAllCars,
+  getUserById,
+  getAllUsersCar,
+  deleteCar
 };
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,14 @@
 const client = require('./client');
-const { addCar } = require(`./index`);
+const { addCar, createUser } = require(`./index`);
+const bcrypt = require("bcrypt");
 
 const dropTables = async() => {
   try {
     await client.query(`
     DROP TABLE IF EXISTS available
+    `)
+    await client.query(`
+    DROP TABLE IF EXISTS users
     `)
   } catch(error) {
     console.log(error);
@@ -23,12 +27,29 @@ const createTables = async() => {
       imageURL TEXT,
       details TEXT NOT NULL,
       price INTEGER NOT NULL
-    )
+    );
+
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+
+    );
     `)
   } catch(error) {
     console.log(error);
   }
 }
+
+const createUsers = async () => {
+  try {
+    const Carlos = await createUser("Carlos", await bcrypt.hash("Contrasena", 10));
+    const Manuel = await createUser("Manuel", await bcrypt.hash("02022022", 10));
+    const Juan = await createUser("Juan", await bcrypt.hash("Pedro22", 10));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const configuracion = async() => {
   await client.connect();
@@ -41,6 +62,10 @@ const configuracion = async() => {
   console.log("CREATED TABLES");
 
   await addCar("2021", "Ford", "F-150", "Raptor", "URL", "Good condition truck", "100000");
+  console.log("ADDED CARS");
+
+  await createUsers();
+  console.log("CREATED USERS");
 
   await client.end();
   console.log("DISCONNECTED");
